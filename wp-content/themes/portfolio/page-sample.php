e0e676259b77e955cf537381ac3718ea16b6983e
Patrick <pmartindesigns@gmail.com>
2018-05-11 00:51:37 -0700

    Santa Ana Content

--- wp-content/themes/portfolio/page-sample.php
+++ wp-content/themes/portfolio/page-sample.php
@@ -37,11 +37,12 @@
 
       <hr/>
 
-      <div class="page-section">
+      <div class="page-section centerize">
         <h2>Design Solutions</h2>
+        <p>After further browsing through the old site, more and more did I realize that it needed more than just an extreme makeover. Below are the issues I discovered and the solutions I crafted along the way.</p>
       </div>
 
-      <p class="centerize">After further browsing through the old site, more and more did I realize that it needed more than just an extreme makeover. Below are the issues I discovered and the solutions I crafted along the way.</p>
+
 
       <div>
         <div class="col-6 issue">
@@ -90,27 +91,28 @@
         </div>
       </div>
 
+
+      <h2 class="text-issue">Poorly-Organized Architecture</h2>
+      <p>Below are some of the examples that showed poor use of architecture:</p>
       <div>
-        <h2 class="text-issue">Poorly-Organized Architecture</h2>
-        <p></p>
-        <p>Below are some of the examples that showed poor use of architecture:</p>
-        <br/>
         <div class="col-6 issue">
           <div class="col">
             <ul>
               <li class="list-item-main"><span class="list-title">Visual disconnection between related pages</span>
-                The main City Hall page (A) is home to the different Department pages, however as shown on the right, a department page (B) has a completely different UI presentation (from typography, to how the sidebar is designed) compared to its 'parent' City Hall page (A).
+                The main City Hall page (A) is home to different department pages, however as shown on the right, the Clerk of the Council (B) department page has a completely different UI presentation (from typography, to how the sidebar is designed) compared to its 'parent' City Hall page (A).
+                <br/><br/>
+                This could remove all perception that either page relates to the other, which significantly misrepresents the underlying hierarchy.
               </li>
               <li class="list-item-main"><span class="list-title">Poor use of breadcrumbs</span>
                 <ul>
-                  <li>The way breadcrumbs are displayed did not stand out to me as a form of navigation due to where it is positioned in the pages, the spacing between each label, and how it is positioned right under the main menu almost like a sub-menu.</li>
+                  <li>The way breadcrumbs are displayed did not immediately stick out to me as a form of navigation due to how it is positioned under the main menu almost like a sub-menu, the big spacing between each breadcrumb label, and how it does not align with the page title.</li>
                   <li>Some pages' breadcrumbs were not accurate with how the page hierarchy is portrayed from the user's perspective.<br/>
                     E.g. As shown on the right, City Hall (A) has the correct breadcrumb structure, however the Clerk of the Council department (B) is missing its parent's breadcrumb label after the 'Home' label and before the 'Clerk of the Council' label.
                   </li>
                 </ul>
               </li>
               <li class="list-item-main"><span class="list-title">Headers</span>
-                I believe page titles are important in presenting page hierarchy. The Clerk of the Council page header (B) looks more prominent than that of the City Hall's, which users may perceive as the more dominant parent page.
+                Page titles can make an impact in presenting page hierarchy. The Clerk of the Council page header (B) looks more prominent than that of the City Hall's, which users may perceive as the more dominant page.
               </li>
             </ul>
           </div>
@@ -128,21 +130,38 @@
 
       <div>
         <h2 class="text-solution">Overlooking the Entire Site</h2>
-        <p></p>
-        <br/>
         <div class="col-6 solution">
           <div class="col">
             <ul>
               <li class="list-item-main"><span class="list-title">Created a Sitemap</span>
+                In order to get a better understanding of the site hierarchy, I created a sitemap which highlighted the key pages and all the subpages that needed to be addressed.<br/>
+                This allowed me to get a better sense of where information will be distributed. It helped in inspiring the design to accomodate to users by ensuring that they are always aware of where they are in the site's hierarchy.
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/sitemap.png" />
               </li>
             </ul>
           </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-6 solution">
           <div class="col">
-            
+            <ul>
+              <li class="list-item-main"><span class="list-title">Categorizing Key Pages</span>
+                After collaborating with our client on the hierarchical issues, we had collectively decided on a design solution: to categorize key pages by their department and make the department section more prominent.
+                <br/>
+                E.g. Below is an example of how a page's hierarchy can be presented in an understandable way. It shows a basic page that belongs to the 'Parks & Recreation' department. In fact, any page that belongs to that department will have the same header.
+                <br/>
+                This refined representation of hierarchy provides clarity about where users are in the site and helps contextualize the content being viewed.
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/dept-category.png" />
+              </li>
+
+              <li class="list-item-main"><span class="list-title">Universal Sidebar that matches with Breadcrumbs</span>
+                I redesigned the old site's sidebar menu to show more hierarchical depth. As a result, users are able to understand better where they are in the pyramid of content, which is helpful for making it more efficient for them to find what they’re looking for or to simply explore the site.
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/sidebar-new.png" />
+                The page's breadcrumbs must have the same hierarchical structure as the sidebar menu. This visual consistency further improves user navigation and minimizes confusion.
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/breadcrumbs.png" />
+              </li>
+            </ul>
+
           </div>
         </div>
       </div>
