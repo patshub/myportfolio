ad23775681f2c6b8317166288dc0c6622a31f873
Patrick <pmartindesigns@gmail.com>
2018-05-12 00:35:25 -0700

    Content Fixes

--- wp-content/themes/portfolio/single-projects-city-of-santa-ana.php
+++ wp-content/themes/portfolio/single-projects-city-of-santa-ana.php
@@ -20,12 +20,10 @@
           <h2>The Project</h2>
           <p>
             The City of Santa Ana website, home to residential information, online services, job opportunities, city-wide alerts, and community-related articles and events,
-            require a full aesthetic makeover to ensure that information is as visually more pleasing as it is accessible.
-            <br/>My advisor at the time started off by redesigning their current homepage with a brand new... brand.
-          </p>
-          <h2>My Job: <strong style="clear:both;display:block;">To Redesign the Interior Pages</strong></h2>
-          <p>After client approval, the project was assigned for me to continue designing the interior pages, using the homepage design as my visual basis.
-            My goal was to ensure that the rest of the site feels as consistently rich and colorful as the approved homepage design.
+            required a full aesthetic makeover to ensure that content is as visually more pleasing as it is accessible.
+            <br/>My advisor at the time came up with a simple redesign of their homepage, which I had helped in refining more towards the client's liking.
+            <br/>After client approval, the project was assigned for me to continue designing the interior pages, using the homepage concept as my visual basis.
+            My goal was to ensure that the rest of the site feels as consistently rich and colorful as how our client envisioned the homepage to be.
           </p>
             <!-- My design advisor provided the design materials and a style guide for me to ensure that the rest of the website feels as rich and colorful as the concepts portrayed in the homepage. -->
         </div>
@@ -46,7 +44,7 @@
       <div class="centerize">
         <h2>Looking Back</h2>
         <p>Even though there was already an established homepage design to kick this project off,
-          it was worth taking a step back and paying the old site a visit in an effort to identify pros and cons in the designs, what could be improved, and to get a sense of the site’s architecture.
+          it was worth taking a step back and paying the old site a visit in an effort to identify visual pros and cons, what could be improved, and to get a sense of the site’s architecture.
         </p>
       </div>
       <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/santa-ana-old.png" />
@@ -55,7 +53,7 @@
       <div class="page-section centerize">
         <h2>Design Solutions</h2>
         <p>After further browsing through the old site, more and more did I realize that it needed more than just an extreme makeover.
-          The in-depth analysis of the site led me to discover many visual issues that needed to be addressed.
+          It led me to discover many fundamental design issues that needed to be addressed.
           Below are the issues I discovered and the solutions I crafted along the way.</p>
       </div>
 
@@ -66,23 +64,23 @@
             <p>Right off the bat, looking over the different pages of the site gave me a sense that it lacked... personality. It didn't have a focus in color, typography, and even the way different elements are laid out. A lot of elements did not look visually compatible with neighboring elements and their environment, which could essentially cause user navigation errors and confusion.</p>
             <p>Below are some of the examples that showed obvious visual inconsistencies:</p>
             <ul>
-              <li class="list-item-main"><span class="list-title">Several colors are used across the website.</span>
+              <li class="list-item-main"><span class="list-title">Several colors are used across the website</span>
                 The site's lack of visual direction can be due to the amount of color being thrown across different pages.
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/colors.png" />
               </li>
-              <li class="list-item-main"><span class="list-title">Typography is being used inconsistently across different pages.</span>
-                The image below is an example of two different pages with inconcsistent representations of information. This greatly reiterates the lack of uniformity across the site.
+              <li class="list-item-main"><span class="list-title">Typography is being used inconsistently across different pages</span>
+                The image below is an example of two different pages with inconsistent representations of information. This greatly reiterates the lack of uniformity across the site.
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/font-stylez.png" />
                 There are about three different font styles being used, and about four varying font sizes. Again, the lack of focus is displayed here.
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/typo-error.png" />
               </li>
-              <li class="list-item-main"><span class="list-title">Content blocks in some pages aren't being displayed the same way in other pages.</span>
-                E.g. There are two versions of the website sidebar showing different font styles, font sizes, and background colors.
+              <li class="list-item-main"><span class="list-title">Visual components that have a similar purpose aren't being displayed the same way</span>
+                E.g. There are two versions of the sidebar menu showing different font styles, font sizes, and background colors.
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/sidebar.png" />
               </li>
               <li class="list-item-main"><span class="list-title">Un-originality</span>
-                Content types that lack a level of originality or uniqueness - relative to other content types - lose their credibility and devalues the information they represent.<br/>
-                E.g. The blocks of content below would be difficult to perceive as three separate event items because of their lack of decoration and visual exclusiveness when compared to other content types.
+                Content types that lack a level of originality or uniqueness - relative to other content types - devalues the information they represent and makes it difficult for users to find distinctions between them.<br/>
+                E.g. The blocks of content below would be difficult to perceive as three separate events because they lack decoration and visual exclusivity.
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/events_old.png" />
               </li>
             </ul>
@@ -97,16 +95,16 @@
             <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/styleguide.png" />
             <p>Creating a style guide ensures that:</p>
             <ul>
-              <li class="list-item-main"><span class="list-title">The website is consistent.</span>
+              <li class="list-item-main"><span class="list-title">The website is consistent</span>
                 The style guide reassures that any particular element's visual representation (e.g. headers, paragraphs, lists, tables, form fields, etc.) will be emulated consistently across the website.
               </li>
-              <li class="list-item-main"><span class="list-title">The website is personalized.</span>
+              <li class="list-item-main"><span class="list-title">The website is personalized</span>
                 The website acquires a brand and a sense of identity that is built upon steady and minimal use of color, typography, layout, and imagery.
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/santa-ana-cut.png" />
               </li>
-              <li class="list-item-main"><span class="list-title">Each type of content gets its own brand, too.</span>
+              <li class="list-item-main"><span class="list-title">Each type of content gets its own brand, too</span>
                 Different content types have their own visual representations that users could easily recognize by spotting distinct elements exclusive to a particular type of content.<br/>
-                E.g. There is a clear visual distinction between a list of event items (A) and a list of articles (B). This disimilarity allows users to meaningfully recognize one content type from the another, preventing confusion.
+                E.g. There is a clear visual distinction between a list of event items (A) and a list of articles (B). This disimilarity allows users to meaningfully distinguish one content type from another, which improves user recognition.
                 <br/>
                 <strong>A.</strong> Events Listing
                 <img src="<?php bloginfo('template_url'); ?>/assets/images/projects/santa-ana/events.png" />
@@ -126,20 +124,25 @@
           <div class="col">
             <ul>
               <li class="list-item-main"><span class="list-title">Visual disconnection between related pages</span>
-                The main City Hall page (A) is home to different department pages, however as shown on the right, the Clerk of the Council (B) department page has a completely different UI presentation (from typography, to how the sidebar is designed) compared to its 'parent' City Hall page (A).
+                The main City Hall page (A) is home to different department pages. However, the Clerk of the Council (B) department page has a completely different UI presentation (from typography, to how the sidebar is designed) compared to its 'parent' City Hall page (A).
                 <br/><br/>
                 This could remove all perception that either page relates to the other, which significantly misrepresents the underlying hierarchy.
               </li>
               <li class="list-item-main"><span class="list-title">Poor use of breadcrumbs</span>
-                <ul>
-                  <li>The way breadcrumbs are displayed did not immediately stick out to me as a form of navigation due to how it is positioned under the main menu almost like a sub-menu, the big spacing between each breadcrumb label, and how it does not align with the page title.</li>
-                  <li>Some pages' breadcrumbs were not accurate with how the page hierarchy is portrayed from the user's perspective.<br/>
-                    E.g. As shown on the right, City Hall (A) has the correct breadcrumb structure, however the Clerk of the Council department (B) is missing its parent's breadcrumb label after the 'Home' label and before the 'Clerk of the Council' label.
-                  </li>
-                </ul>
+                  The way breadcrumbs are presented does not show evidence of its navigational nature.
+                  <ul>
+                    <li>The dot between each label does not imply any directional indication. </li>
+                    <li>The linked breadcrumb label (e.g. 'Home') is shown to be more prominent than the actual page's breadcrumb label ('City Hall').</li>
+                    <li>There is quite a big spacing between the labels, which highlights the lack of proximity that is meant to group them together.</li>
+                    <li>The grey background color behind the breadcrumbs separates them from the body of content.</li>
+
+                  </ul>
+
+                  Furthermore, some pages' breadcrumbs are not accurate with how the hierarchy is portrayed from the user's perspective.<br/>
+                  For example, if we assume that the City Hall page (A) has the correct breadcrumb structure, then the Clerk of the Council department (B) is missing its parent's breadcrumb label after the 'Home' label and before the 'Clerk of the Council' label.
               </li>
               <li class="list-item-main"><span class="list-title">Headers</span>
-                Page titles can make an impact in presenting page hierarchy. The Clerk of the Council page header (B) looks more prominent than that of the City Hall's, which users may perceive as the more dominant page.
+                Page titles can make an impact in presenting page hierarchy. The Clerk of the Council page header (B) looks more prominent than that of the City Hall's, which users may perceive as the more dominant page. 
               </li>
             </ul>
           </div>
@@ -203,16 +206,16 @@
                 I've learned that it is important for any kind of website to have a visual focus that users can easily familiarize with.
                 To achieve this, the designer's responsibility is:
                 <ul>
-                  <li>to incorporate a level of consistency or uniformity on every element being used</li>
+                  <li>to incorporate a high level of consistency and uniformity on all visual components</li>
                   <li>to use color, typography, and imagery effectively so that they compliment one another rather than create distractions</li>
                   <li>to grant key elements or content types their own exclusive visual identity.
                     This can help users make distinctions between different types of content.</li>
                 </ul>
               </li>
               <li class="list-item-main"><span class="list-title">Overlooking the Entire Website is Important</span>
-                It is easy to just get started designing several pages without accounting for how they relate to one another.
-                However, I've learned that it is important to first take a step back and study the whole sitemap to understand
-                how pages are connected with one another in the hierarchy - an understanding that is ultimately reflected on how the pages will be designed.
+                It is easy to just get started designing page by page without addressing the entire scope of a site.
+                However, I've learned that it is important to first take a step back and visualize how pages
+                are connected with one another in the hierarchy - an understanding that is ultimately reflected on how the pages will be designed.
               </li>
             </ul>
           </div>
@@ -222,11 +225,10 @@
           <div class="col">
             <ul>
               <li class="list-item-main"><span class="list-title">Have Creative Control</span>
-                It can be tempting to think there are no challenges in using an existing template as basis to design an entire website.
-                However, it was a challenging task for me to limit my tendencies to 'overdesign'.
-                In other words, I could juice out the peak of my creativity but only within the constraints of an already-established design (presented as the Homepage for this project).
-                <br/>This was quite a learning curve for me because I always had a mindset of designing a website to have everything mashed together in a bowl, fully-loaded with all the features that can be thought of.
-                But that's where the website loses it's personality - it tries to be everything it's not, rather than be one thing that it is.
+                It was a challenging task for me to limit my tendencies to 'overdesign'.
+                Because I didn't start the project with a completely empty canvas, I had to be able to utilize the peak of my creativity strictly within the constraints of an already-established concept (presented as the homepage for this project).
+                <br/>This was quite a learning curve for me because I always had the tendency to over-populate a website with several features to the point where the aesthetics became very 'busy'.
+                I've realized that that's where the website loses it's personality - it tries to be everything it's not, rather than be one thing that it is.
               </li>
             </ul>
           </div>
